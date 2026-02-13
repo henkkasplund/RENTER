8c0a0f97dd732e159c1be11dc860edb38a85796b
Henrik Asplund <232446152+henkkasplund@users.noreply.github.com>
2026-02-13 22:04:56 +0200

    forgot to add listings

--- listings.py
+++ listings.py
@@ -5,19 +5,19 @@ import re
 def add_listing(user_id, listing_data):
     sql = """INSERT INTO listings (user_id, rooms, size, rent,
                                 address, postcode,floor, floors, sauna, balcony,
-                                bath, elevator, laundry, cellar, pool, description)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+                                bath, elevator, laundry, cellar, pool, description,
+                                municipality_id, condition_id, property_type_id)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
-    db.execute(sql, [user_id, listing_data["rooms"], listing_data["size"], listing_data["rent"],
-                     listing_data["address"], listing_data["postcode"],listing_data["floor"],
-                     listing_data["floors"], listing_data["sauna"], listing_data["balcony"],
-                     listing_data["bath"], listing_data["elevator"], listing_data["laundry"],
-                     listing_data["cellar"], listing_data["pool"], listing_data["description"]])
-    listing_id = db.last_insert_id()
-    sql1 = "INSERT INTO listing_classes (listing_id, title, value) VALUES (?, ?, ?)"
-    db.execute(sql1, [listing_id, "municipality", listing_data["municipality"]])
-    db.execute(sql1, [listing_id, "condition", listing_data["condition"]])
-    db.execute(sql1, [listing_id, "property_type", listing_data["property_type"]])
+    db.execute(sql, [user_id, listing_data["rooms"], listing_data["size"],
+                     listing_data["rent"], listing_data["address"],
+                     listing_data["postcode"],listing_data["floor"],
+                     listing_data["floors"], listing_data["sauna"],
+                     listing_data["balcony"], listing_data["bath"],
+                     listing_data["elevator"], listing_data["laundry"],
+                     listing_data["cellar"], listing_data["pool"],
+                     listing_data["description"], listing_data["municipality_id"],
+                     listing_data["condition_id"], listing_data["property_type_id"]])
 
 def get_listings():
     sql = """SELECT listings.id,
@@ -26,8 +26,7 @@ def get_listings():
                     listings.size,
                     m.value AS municipality
              FROM listings
-             LEFT JOIN listing_classes m
-                ON m.listing_id = listings.id AND m.title = 'municipality'
+             JOIN classes m ON m.id = listings.municipality_id
              ORDER BY listings.id DESC"""
     return db.query(sql)
 
@@ -35,23 +34,23 @@ def get_listing(listing_id):
     sql = """SELECT listings.*,
                     users.id AS user_id,
                     users.username,
+                    m.id AS municipality_id,
                     m.value AS municipality,
+                    c.id AS condition_id,
                     c.value AS condition,
+                    p.id AS property_type_id,
                     p.value AS property_type
-             FROM listings
-             JOIN users ON listings.user_id = users.id
-             LEFT JOIN listing_classes m
-                ON m.listing_id = listings.id AND m.title = 'municipality'
-             LEFT JOIN listing_classes c
-                ON c.listing_id = listings.id AND c.title = 'condition'
-             LEFT JOIN listing_classes p
-                ON p.listing_id = listings.id AND p.title = 'property_type'
-             WHERE listings.id = ?"""
+            FROM listings
+            JOIN users ON users.id = listings.user_id
+            JOIN classes m ON m.id = listings.municipality_id
+            JOIN classes c ON c.id = listings.condition_id
+            JOIN classes p ON p.id = listings.property_type_id
+            WHERE listings.id = ?"""
     result = db.query(sql, [listing_id])
     return result[0] if result else None
 
 def get_classes(title):
-    sql = "SELECT value FROM classes WHERE title = ?"
+    sql = "SELECT id, value FROM classes WHERE title = ? ORDER BY id"
     return db.query(sql, [title])
 
 def get_form_data():
@@ -69,8 +68,8 @@ def get_form_data():
         abort(403)
     if not re.search("^[1-9][0-9]{0,4}$", rent):
         abort(403)
-    municipality = request.form["municipality"]
-    if not municipality or len(municipality) > 30:
+    municipality_id = request.form["municipality_id"]
+    if not re.search("^[0-9]+$", municipality_id):
         abort(403)
     address = request.form["address"]
     if not address or len(address) > 50:
@@ -90,11 +89,11 @@ def get_form_data():
         abort(403)
     if not re.search("^[1-9][0-9]?$", floors):
         abort(403)
-    condition = request.form["condition"]
-    if not condition or len(condition) > 30:
+    condition_id = request.form["condition_id"]
+    if not re.search("^[0-9]+$", condition_id):
         abort(403)
-    property_type = request.form["property_type"]
-    if not property_type or len(condition) > 30:
+    property_type_id = request.form["property_type_id"]
+    if not re.search("^[0-9]+$", property_type_id):
         abort(403)
     sauna = 1 if "sauna" in request.form else 0
     balcony = 1 if "balcony" in request.form else 0
@@ -110,13 +109,13 @@ def get_form_data():
         "rooms": rooms,
         "size": size,
         "rent": rent,
-        "municipality": municipality,
+        "municipality_id": municipality_id,
         "address": address,
         "postcode": postcode,
         "floor": floor,
         "floors": floors,
-        "condition": condition,
-        "property_type": property_type,
+        "condition_id": condition_id,
+        "property_type_id": property_type_id,
         "sauna": sauna,
         "balcony": balcony,
         "bath": bath,
@@ -131,25 +130,20 @@ def update_listing(listing_id, listing_data):
     sql = """UPDATE listings SET rooms = ?, size = ?, rent = ?,
                     address = ?, postcode = ?, floor = ?, floors = ?,
                     sauna = ?, balcony = ?, bath = ?, elevator = ?, laundry = ?,
-                    cellar = ?, pool = ?, description = ?
+                    cellar = ?, pool = ?, description = ?, municipality_id = ?,
+                    condition_id = ?, property_type_id = ?
             WHERE id = ?"""
-    db.execute(sql, [listing_data["rooms"], listing_data["size"], listing_data["rent"],
-                     listing_data["address"], listing_data["postcode"], listing_data["floor"],
-                     listing_data["floors"], listing_data["sauna"], listing_data["balcony"],
-                     listing_data["bath"], listing_data["elevator"], listing_data["laundry"],
-                     listing_data["cellar"], listing_data["pool"], listing_data["description"], listing_id])
-    sql1 = """DELETE FROM listing_classes
-                 WHERE listing_id = ?
-                 AND title IN ('municipality','condition','property_type')"""
-    db.execute(sql1, [listing_id])
-
-    sql2 = "INSERT INTO listing_classes (listing_id, title, value) VALUES (?, ?, ?)"
-    db.execute(sql2, [listing_id, "municipality", listing_data["municipality"]])
-    db.execute(sql2, [listing_id, "condition", listing_data["condition"]])
-    db.execute(sql2, [listing_id, "property_type", listing_data["property_type"]])
+    db.execute(sql, [listing_data["rooms"], listing_data["size"],
+                     listing_data["rent"], listing_data["address"],
+                     listing_data["postcode"], listing_data["floor"],
+                     listing_data["floors"], listing_data["sauna"],
+                     listing_data["balcony"], listing_data["bath"],
+                     listing_data["elevator"], listing_data["laundry"],
+                     listing_data["cellar"], listing_data["pool"],
+                     listing_data["description"], listing_data["municipality_id"],
+                     listing_data["condition_id"], listing_data["property_type_id"],listing_id])
 
 def remove_listing(listing_id):
-    db.execute("DELETE FROM listing_classes WHERE listing_id = ?", [listing_id])
     db.execute("DELETE FROM listings WHERE id = ?", [listing_id])
 
 def search_listings(query):
@@ -162,9 +156,9 @@ def search_listings(query):
                     m.value AS municipality
              FROM listings
              JOIN users ON listings.user_id = users.id
-             LEFT JOIN listing_classes m
-                ON m.listing_id = listings.id AND m.title = 'municipality'
+             JOIN classes m ON m.id = listings.municipality_id
              WHERE m.value LIKE ? OR listings.description LIKE ?
              ORDER BY listings.id DESC"""
+
     result = "%" + query + "%"
     return db.query(sql, [result, result])
